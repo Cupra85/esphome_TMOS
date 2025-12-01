5b4cf2118b46372147550e0d6f07b3cbda5d7537
Cupra85 <daniel.curten@gmail.com>
2025-12-01 20:49:10 +0100

    Update __init__.py

--- components/tmos/__init__.py
+++ components/tmos/__init__.py
@@ -1,1 +1,17 @@
 
+import esphome.config_validation as cv
+from esphome.components import sensor as sensor_platform
+from esphome.components import binary_sensor as binary_sensor_platform
+
+from .sensor import (
+    TMOS_SENSOR_SCHEMA,
+    TMOS_BINARY_SCHEMA,
+    to_code_sensor,
+    to_code_binary,
+)
+
+# register both platforms under the name "tmos"
+sensor_platform.register_sensor_platform("tmos", TMOS_SENSOR_SCHEMA, to_code_sensor)
+binary_sensor_platform.register_binary_sensor_platform(
+    "tmos", TMOS_BINARY_SCHEMA, to_code_binary
+)
