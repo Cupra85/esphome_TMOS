34d9e0b919a26a9eac3567c7c6ee0b9e19c3b105
Cupra85 <daniel.curten@gmail.com>
2025-12-01 20:55:26 +0100

    Update sensor.py

--- components/tmos/sensor.py
+++ components/tmos/sensor.py
@@ -3,16 +3,21 @@ import esphome.config_validation as cv
 
 from esphome.components import i2c, sensor, binary_sensor
 
+
 TMOS_NS = cg.esphome_ns.namespace("TMOS")
 TMOSComponent = TMOS_NS.class_("TMOSComponent", cg.PollingComponent, i2c.I2CDevice)
 
-# ---------------- SENSOR PLATFORM ----------------
+
+# -----------------------------------------------------
+# SENSOR PLATFORM (temperatures)
+# -----------------------------------------------------
 
 TMOS_SENSOR_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(TMOSComponent),
 
     cv.Optional("ambient_temperature"): sensor.sensor_schema(),
     cv.Optional("object_temperature"): sensor.sensor_schema(),
+
 }).extend(i2c.i2c_device_schema(None))
 
 
@@ -29,8 +34,9 @@ async def to_code_sensor(config):
         s = await sensor.new_sensor(config["object_temperature"])
         cg.add(comp.set_object_temperature_sensor(s))
 
-
-# ---------------- BINARY SENSOR PLATFORM ----------------
+# -----------------------------------------------------
+# BINARY SENSOR PLATFORM
+# -----------------------------------------------------
 
 TMOS_BINARY_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(TMOSComponent),
@@ -38,6 +44,7 @@ TMOS_BINARY_SCHEMA = cv.Schema({
     cv.Optional("presence"): binary_sensor.binary_sensor_schema(),
     cv.Optional("motion"): binary_sensor.binary_sensor_schema(),
     cv.Optional("thermal_shock"): binary_sensor.binary_sensor_schema(),
+
 }).extend(i2c.i2c_device_schema(None))
 
 
