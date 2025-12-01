2ce74ea23f8e23d689792e65fc349d6cea69b639
Cupra85 <daniel.curten@gmail.com>
2025-12-01 20:58:31 +0100

    Update sensor.py

--- components/tmos/sensor.py
+++ components/tmos/sensor.py
@@ -1,66 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-
-from esphome.components import i2c, sensor, binary_sensor
-
+from esphome.components import sensor, i2c
 
 TMOS_NS = cg.esphome_ns.namespace("TMOS")
 TMOSComponent = TMOS_NS.class_("TMOSComponent", cg.PollingComponent, i2c.I2CDevice)
 
-
-# -----------------------------------------------------
-# SENSOR PLATFORM (temperatures)
-# -----------------------------------------------------
-
 TMOS_SENSOR_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(TMOSComponent),
-
-    cv.Optional("ambient_temperature"): sensor.sensor_schema(),
-    cv.Optional("object_temperature"): sensor.sensor_schema(),
-
-}).extend(i2c.i2c_device_schema(None))
+    cv.Required("ambient_temperature"): sensor.sensor_schema(),
+    cv.Required("object_temperature"): sensor.sensor_schema(),
+}).extend(i2c.i2c_device_schema(0x5A))
 
 
-async def to_code_sensor(config):
-    comp = cg.new_Pvariable(config["id"])
-    await cg.register_component(comp, {})
-    await i2c.register_i2c_device(comp, config)
+async def to_code(config):
+    var = cg.new_Pvariable(config["id"])
+    await cg.register_component(var, {})
+    await i2c.register_i2c_device(var, config)
 
-    if "ambient_temperature" in config:
-        s = await sensor.new_sensor(config["ambient_temperature"])
-        cg.add(comp.set_ambient_temperature_sensor(s))
+    ambient = await sensor.new_sensor(config["ambient_temperature"])
+    cg.add(var.set_ambient_temperature_sensor(ambient))
 
-    if "object_temperature" in config:
-        s = await sensor.new_sensor(config["object_temperature"])
-        cg.add(comp.set_object_temperature_sensor(s))
-
-# -----------------------------------------------------
-# BINARY SENSOR PLATFORM
-# -----------------------------------------------------
-
-TMOS_BINARY_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(TMOSComponent),
-
-    cv.Optional("presence"): binary_sensor.binary_sensor_schema(),
-    cv.Optional("motion"): binary_sensor.binary_sensor_schema(),
-    cv.Optional("thermal_shock"): binary_sensor.binary_sensor_schema(),
-
-}).extend(i2c.i2c_device_schema(None))
+    object_temp = await sensor.new_sensor(config["object_temperature"])
+    cg.add(var.set_object_temperature_sensor(object_temp))
 
 
-async def to_code_binary(config):
-    comp = cg.new_Pvariable(config["id"])
-    await cg.register_component(comp, {})
-    await i2c.register_i2c_device(comp, config)
-
-    if "presence" in config:
-        b = await binary_sensor.new_binary_sensor(config["presence"])
-        cg.add(comp.set_presence_sensor(b))
-
-    if "motion" in config:
-        b = await binary_sensor.new_binary_sensor(config["motion"])
-        cg.add(comp.set_motion_sensor(b))
-
-    if "thermal_shock" in config:
-        b = await binary_sensor.new_binary_sensor(config["thermal_shock"])
-        cg.add(comp.set_thermal_shock_sensor(b))
+# REGISTER PLATFORM
+sensor.register_sensor_platform("tmos", TMOS_SENSOR_SCHEMA, to_code)
